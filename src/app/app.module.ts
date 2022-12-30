f6a3a6ad991ead71a299d5fb95951d9ec76a6663
royBrocken <rdbrockman@gmail.com>
2022-12-30 12:03:06 +0000

    added create article form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ArticleListItemComponent } from './articles/articles-list/article-list-
 import { ArticlesListComponent } from './articles/articles-list/articles-list.component';
 import { MenuComponent } from './menu/menu.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,13 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     DashboardComponent,
     MenuComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/articles/article-create/article-create.component.ts
+++ src/app/articles/article-create/article-create.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-article-create',
   templateUrl: './article-create.component.html',
 })
-export class ArticleCreateComponent {
+export class ArticleCreateComponent implements OnInit {
+  articleForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.articleForm = new FormGroup({
+      title: new FormControl<string | null>(null, Validators.required),
+      source: new FormControl<string | null>(null, [Validators.required]),
+      summary: new FormControl<string | null>(null, [Validators.required]),
+    });
+  }
+  
+  controlHasError(controlName: string, errorProperty: string, errorOnValue: boolean | string | number = true): boolean {
+    const { errors, touched } = this.articleForm.controls[controlName];
+
+    if (!touched || errors == null) return false; //>>>
+
+    return errors[errorProperty] === errorOnValue;
+
+  }
+
+  createArticle(): void {
+    if (this.articleForm.invalid) return this.articleForm.markAllAsTouched(); //>>>
+
+    console.log(this.articleForm.value)
+  }
 }
